fbf14ead073fbaa1f91d8cf8bf8c9c468dd68059
Roman2dot0 <test.testovich5@yandex.ru>
2018-01-17 17:29:11 +0300

    For CreativeCallProject

---
+++ file_to_mail.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+
+import smtplib as smtp
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+import requests
+import configparser
+from datetime import date
+
+
+path = "settings.ini"
+config = configparser.ConfigParser()
+config.read(path)
+
+hlogin = config.get("Settings", "hlogin")
+hpass = config.get("Settings", "hpass")
+url = config.get("Settings", "url")
+
+f_date = date.strftime(date.today(), ("%Y%m%d"))
+f_name = int(f_date) - 3
+h_filename = "bknProfi.{}.xlsx".format(f_name)
+s_file = '{}.xlsx'.format(f_name)
+http = url + h_filename
+
+request = requests.get(http, auth=(hlogin, hpass))
+
+with open(s_file, 'wb') as f:
+    f.write(request.content)
+f.close()
+
+
+email = config.get("Settings", "email")
+password = config.get("Settings", "password")
+dest_email = config.get("Settings", "dest_email")
+subject = "БД от Реалиста"
+email_text = "В вложении"
+
+message = MIMEMultipart()
+
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open(s_file, "rb").read())
+part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(s_file))
+encoders.encode_base64(part)
+
+
+message['From'] = email
+message["To"] = dest_email
+message["Subject"] = subject
+message.attach(MIMEText(email_text))
+message.attach(part)
+
+server = smtp.SMTP_SSL('smtp.yandex.com')
+server.ehlo(email)
+server.login(email, password)
+server.auth_plain()
+server.sendmail(email, dest_email, message.as_bytes())
+server.quit()
